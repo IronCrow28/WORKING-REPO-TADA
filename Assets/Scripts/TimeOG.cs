adafee17173a8299abde1d35964ad68ad06951e5
IronCrow28 <104455374+IronCrow28@users.noreply.github.com>
2022-05-16 09:41:41 -0700

    Time
    tiny

---
+++ Assets/Scripts/TimeOG.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeOG : MonoBehaviour
+{
+    public float timeValue = 90;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeValue -= Time
+    }
+}
