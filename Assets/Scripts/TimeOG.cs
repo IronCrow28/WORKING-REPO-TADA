3905268e2fb816988a4e89b2f5deee79d6bfc2d9
IronCrow28 <104455374+IronCrow28@users.noreply.github.com>
2022-05-24 10:44:26 -0700

    horse
    ae

--- Assets/Scripts/TimeOG.cs
+++ Assets/Scripts/TimeOG.cs
@@ -1,14 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeOG : MonoBehaviour
 {
-    public float timeValue = 90;
+    public Text timerText;
 
-    // Update is called once per frame
-    void Update()
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+
+    private float elapsedTime;
+
+    private void Start()
     {
-        
+        timerText.text = "00:00";
+        timerGoing = false;
+
+        BeginTimer();
+    }
+
+    public void BeginTimer()
+    {
+        timerGoing = true;
+        elapsedTime = 0f;
+
+        StartCoroutine(UpdateTimer());
+    }
+
+    public void EndTimer()
+    {
+        timerGoing = false;
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = timePlaying.ToString("mm':'ss");
+            timerText.text = timePlayingStr;
+            yield return null;
+        }
     }
 }
